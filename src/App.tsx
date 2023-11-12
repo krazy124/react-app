0d40bbb23254d568ed7ad51d5d8ea6db21f635b1
crazy woodwork <william.meredith4@gmail.com>
2023-11-12 02:42:36 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import Cat from './components/button';
+import Cat from "./components/button";
 
 function App() {
-  const aConsole = () => console.log('It Clicked!!');
+  const aConsole = () => console.log("It Clicked!!");
   let btnColor = "warning";
-  let firstName = "William";
-  let alName = 'alert alert-warning alert-dismissible fade show';
-  let alMessage = 'You should check in on some of those fields below.';
-  let caName = 'btn-close';
+  let firstName = "John";
+  let alName = "alert alert-warning alert-dismissible fade show";
+  let alMessage = "You should check in on some of those fields below.";
+  let caName = "btn-close";
 
   return (
-    <Cat alertName={alName} alertMessage={alMessage} cName={caName} color={btnColor} onClick={aConsole}>{firstName}</Cat>
+    <Cat
+      alertName={alName}
+      alertMessage={alMessage}
+      cName={caName}
+      color={btnColor}
+      onClick={aConsole}
+    >
+      {firstName}
+    </Cat>
   );
 }
 
 export default App;
-
