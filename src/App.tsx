62d4f73f76af2d0dec8aaafc6fa41b7cb29e1aa4
crazy woodwork <william.meredith4@gmail.com>
2023-11-13 01:05:32 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Cat from "./components/button";
 function App() {
   const aConsole = () => console.log("It Clicked!!");
   let btnColor = "warning";
-  let firstName = "John";
+  let firstName = "Skippy";
   let alName = "alert alert-warning alert-dismissible fade show";
   let alMessage = "You should check in on some of those fields below.";
   let caName = "btn-close";
